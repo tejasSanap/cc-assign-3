892497cac08015467254cc0af5e3185f4267e4f9
vr513 <varadmanraj@gmail.com>
2023-02-13 22:56:01 +0530

    final

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/cloud-computing',(req,res) => {
+    res.send('I love Cloud Computing and Devops')
+})
+
+app.get('/student-details',(req,res) => {
+    res.send('I am Varad Rajopadhye of TY - IT A , Roll no : 331048 and PRN  : 22010084')
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Assignment app listening on port ${port}`)
 })
